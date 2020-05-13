8728a11299a44f79dc64f600419a9e84957c6529
braja01 <56359306+braja01@users.noreply.github.com>
2020-05-13 21:31:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -310,7 +310,7 @@ def bot_with_cyclic_update_process(update_event, answer_scores):
     upd_thread.start()
 
     loop = asyncio.get_event_loop()
-    loop.create_task(bot.start('bot token'))
+    loop.create_task(bot.start('Generate bot token'))
     loop.run_forever()
 
 
